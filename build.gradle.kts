754dbe3de60b73f74b323f231a213bab4caf967b
thedarkarchitect <romanreynolds240@gmail.com>
2023-09-07 15:15:23 +0300

    Starter code

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("nav_version", "2.5.7")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+}
